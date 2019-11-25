78cf46ad629d178833cd62407ee83e86c5df1274
Justin Carter <justin@misthos.io>
2019-11-25 15:32:59 +0100

    Update e2e script to build bisq

--- ci/tasks/test-e2e.sh
+++ ci/tasks/test-e2e.sh
@@ -4,7 +4,12 @@ set -eu
 
 export CARGO_HOME="$(pwd)/cargo-home"
 export CARGO_TARGET_DIR="$(pwd)/cargo-target-dir"
-export RISQ_BIN_DIR=${CARGO_TARGET_DIR}/debug
+export BISQ_CLI_BIN_DIR=${CARGO_TARGET_DIR}/debug
+export BISQ_SRC_DIR="$(pwd)/bisq-repo"
+
+pushd ${BISQ_SRC_DIR}
+./gradlew daemon:build
+popd
 
 pushd repo
 
