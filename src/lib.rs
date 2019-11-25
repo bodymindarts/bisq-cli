9cd447a57b1cf56e4f6f260be5545f924629a048
Justin Carter <justin@misthos.io>
2019-11-25 13:32:27 +0100

    Add fail-on-warnings feature for ci

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+#![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
+#![cfg_attr(feature = "fail-on-warnings", deny(clippy::all))]
+#![allow(clippy::large_enum_variant)]
+#![allow(clippy::too_many_arguments)]
+
 pub mod cli;
 mod client;
